032c4450d87150b725a519c0f789cdc952973400
Tomasz <estewez@gmail.com>
2018-01-25 21:07:46 +0100

    grouped dispatch functions into one object

--- src/Comment.js
+++ src/Comment.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import './Comment.css';
 
-const Comment = ({text, votes, id, thumbUpComment, thumbDownComment, removeComment, editComment}) =>
+const Comment = ({text, votes, id, action}) =>
     <li className='Item list-group-item'>
-        <a className='Left fa fa-caret-left' onClick={() => thumbDownComment(id)}></a>
+        <a className='Left fa fa-caret-left' onClick={() => action.thumbDownComment(id)}></a>
         <span className='Votes'>{votes} </span>
-        <a className='Right fa fa-caret-right' onClick={() => thumbUpComment(id)}></a> 
+        <a className='Right fa fa-caret-right' onClick={() => action.thumbUpComment(id)}></a> 
         <p>{text}</p>
         <div className='Clickers'>
-            <a className='Clicker fa fa-times' onClick={() => removeComment(id)}></a>
-            <a className='Clicker fa fa-pencil-square-o' onClick={() => editComment(id, text)}></a>
+            <a className='Clicker fa fa-times' onClick={() => action.removeComment(id)}></a>
+            <a className='Clicker fa fa-pencil-square-o' onClick={() => action.editComment(id, text)}></a>
         </div>
         <p>{''}</p>
     </li>;
--- src/CommentContainer.js
+++ src/CommentContainer.js
@@ -6,10 +6,12 @@ import {removeComment} from './actions.js';
 import {editComment} from './actions.js';
 
 const mapDispatchToProps = dispatch => ({
-    thumbUpComment: (id) => dispatch(thumbUpComment(id)),
-    thumbDownComment: (id) => dispatch(thumbDownComment(id)),
-    removeComment: (id) => dispatch(removeComment(id)),
-    editComment: (id, text) => dispatch(editComment(id, text))
+    action: {
+        thumbUpComment: (id) => dispatch(thumbUpComment(id)),
+        thumbDownComment: (id) => dispatch(thumbDownComment(id)),
+        removeComment: (id) => dispatch(removeComment(id)),
+        editComment: (id, text) => dispatch(editComment(id, text))
+    }
 });
 
 export default connect(null, mapDispatchToProps)(Comment);
